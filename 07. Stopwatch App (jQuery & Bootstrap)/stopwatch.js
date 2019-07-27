290047f4971c22f5fc181a8102fb6e76c6a5a57d
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2019-07-27 19:48:22 +0300

    07. App Logic

---
+++ 07. Stopwatch App (jQuery & Bootstrap)/stopwatch.js
@@ -0,0 +1,35 @@
+$(function () {
+    //variables
+    // App mode 
+    // time counter
+    // lap counter
+    // variable for setInterval
+    // Number of Laps
+    // minutes, seconds, centiseconds for time and lap
+
+    // On App load show start and lap buttons
+    // click n startButton
+    // mode on
+    // show stop and lap buttons
+    // start counter
+
+    // click on stopButton
+    //   show resume and reset buttons
+    //   stop counter
+
+    // click on resumeButton
+    //   show stop and lap buttons
+    //   start action
+
+    // click on resetButton
+    //   reload the page
+
+    // click on lapButton
+    //   if mode is On
+    //     stop action
+    //     resetLap and print lap details
+    //     start action
+
+    // functions
+
+});
