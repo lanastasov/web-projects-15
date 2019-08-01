04fdfeaa80e098028c43bb1b711d1cedb085b0a6
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2019-08-01 14:54:00 +0300

    08. Canvas

--- 08. Drawing App (jQuery, Canvas)/javascript.js
+++ 08. Drawing App (jQuery, Canvas)/javascript.js
@@ -7,4 +7,28 @@ $(function () {
             $("#circle").width(ui.value);
         }
     });
+
+    var canvas = document.getElementById("paint");
+    var context = canvas.getContext('2d');
+
+    // draw a line
+    // declare new path
+    context.beginPath();
+    // set line width
+    context.lineWidth = 40;
+    // set line color
+    context.strokeStyle = '#42e565';
+    //set cap to the line (round, butt, square)
+    context.lineCap = "round";
+    // set line join styleMedia(bevel, round, miter)
+    context.lineJoin = "round";
+
+    context.moveTo(50, 50);
+    // draw a straight line from starting point to a new position
+    context.lineTo(200, 200);
+
+    context.lineTo(400, 100);
+
+    //make line visible
+    context.stroke();
 });
