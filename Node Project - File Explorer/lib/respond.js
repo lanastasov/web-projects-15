2df0f40ebdbea41bba794d73cfcd96dd1cf83d4d
lanastasov <lubomir.anastasov@gmail.com>
2020-01-23 18:57:22 +0200

    decode pathname

--- Node Project - File Explorer/lib/respond.js
+++ Node Project - File Explorer/lib/respond.js
@@ -1,6 +1,29 @@
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+const staticBasePath = path.join(__dirname, '..','static');
+
 const respond = (request, response) => {
-    response.write('respond fired');
-    response.end();
+    
     // console.log('respond fired');
+    let pathname = url.parse(request.url, true).pathname;
+
+    if(pathname === '/favicon.ico') {
+        return false;
+    }
+    console.log(pathname);
+
+    pathname = decodeURIComponent(pathname);
+
+    const fullStaticPath = path.join(staticBasePath, pathname);
+
+    if(!fs.existsSync(fullStaticPath)) {
+        console.log(`${fullStaticPath} does not exist`);
+        response.write('404: File not found!');
+        response.end();
+    }else {
+        response.write('File found!');
+        response.end();
+    }
 }
 module.exports = respond;
