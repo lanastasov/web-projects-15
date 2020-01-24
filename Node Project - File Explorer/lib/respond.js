44f8022d37784ac44dbc9ed24eee679938f67e79
lanastasov <lubomir.anastasov@gmail.com>
2020-01-24 10:40:28 +0200

    display folder content + print the title

--- Node Project - File Explorer/lib/respond.js
+++ Node Project - File Explorer/lib/respond.js
@@ -21,9 +21,28 @@ const respond = (request, response) => {
         console.log(`${fullStaticPath} does not exist`);
         response.write('404: File not found!');
         response.end();
-    }else {
-        response.write('File found!');
-        response.end();
+        return false;
     }
+
+    let stats;
+    try {
+        stats = fs.lstatSync(fullStaticPath)
+    } catch(err) {
+        console.log(`lstatsync Error: ${err}`);
+    }
+
+    if(stats.isDirectory()) {
+        let data = fs.readFileSync(path.join(staticBasePath, 'project_files/index.html'), 'utf-8');
+        console.log(pathname);
+        let pathElements = pathname.split('/').reverse();
+        pathElements = pathElements.filter(element => element !== '');
+        const folderName = pathElements[0];
+        console.log(folderName);
+        data = data.replace('page_title', folderName);
+        response.statusCode = 200;
+        response.write(data);
+        response.end();
+    };
+  
 }
 module.exports = respond;
