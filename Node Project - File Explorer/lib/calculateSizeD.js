82d7ef4ad604da7596a7d72942feff8b10195d50
lanastasov <lubomir.anastasov@gmail.com>
2020-01-25 11:17:00 +0200

    calculate directory size

--- Node Project - File Explorer/lib/calculateSizeD.js
+++ Node Project - File Explorer/lib/calculateSizeD.js
@@ -1,6 +1,22 @@
-const calculateSizeD = itemFullStaticPath => {
+const {execSync} = require('child_process');
 
-    return ['110M', 110*1000*1000];
+const calculateSizeD = itemFullStaticPath => {
+    const itemFullStaticPathCleaned = itemFullStaticPath.replace(/\s/g, '\ ');
+    const commandOutput = execSync(`du - sh "${itemFullStaticPathCleaned}"`).toString();
+
+    console.log(commandOutput);
+
+    let filesize = commandOutput.replace(/\s/g, '');
+    filesize = filesize.split('/');
+    filesize = filesize[0];
+    console.log(filesize);
+    
+    const filesizeUnit = filesize.replace(/\d|\./g, '');
+    console.log(filesizeUnit);
+    const filesizeNumber = filesize.replace(/[a-z]/i/g, '');
+    console.log(filesizeNumber);
+
+    return [filesize, 110*1000*1000];
 };
 
 module.exports = calculateSizeD;
--- Node Project - File Explorer/playground/child_process.js
+++ Node Project - File Explorer/playground/child_process.js
@@ -1,3 +1,11 @@
 const child_process = require('child_process');
 
 console.log(child_process.execSync('pwd').toString());
+
+try{
+    const result = child_process.execSync('pwd').toString();
+    console.log(result);
+
+}catch(err) {
+    console.log(`Error: ${err}`);
+}
